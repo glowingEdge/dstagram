32a198ccc29fb157f99e6ce82b135749b5275fd1
glowingEdge <glowingEdge.com>
2019-05-30 22:26:27 +0900

    photo admin 설정

--- photo/models.py
+++ photo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 class Photo(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_photos')
@@ -10,3 +12,9 @@ class Photo(models.Model):
 
     class Meta:
         ordering = ['-updated']
+
+    def __str__(self):
+        return self.author.username + " " + self.created.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_absolute_url(self):
+        return reverse('photo:photo_detail', args=[str(self.id)])
